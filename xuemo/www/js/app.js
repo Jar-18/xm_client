8f69c3c22c99b28ce85f44a80838e57b7d22c0a9
amberonno <amberonno@gmail.com>
2015-07-12 10:37:40 +0800

    聊天页面开发

--- xuemo/www/js/app.js
+++ xuemo/www/js/app.js
@@ -1,83 +1,36 @@
 // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
+angular.module('starter.controllers', [    ])
 
-.config(function($stateProvider, $urlRouterProvider) {
+angular.module('starter', [
+        'ionic',
+        'starter.controllers'
+    ])
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+    .config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-  // Each tab has its own nav history stack:
+            // setup an abstract state for the tabs directive
+            .state('app', {
+                url: "/app",
+                views:{
+                    'app':{
+                        templateUrl: 'module/app/tab/index.html'
+                    }
+                }
+            })
+            .state('app.chat', {
+                url: '/chat',
+                views: {
+                    'chats': {
+                        templateUrl: 'module/app/chat/index.html',
+                        controller: 'chatCtrl'
+                    }
+                }
+            })
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/chat');
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+    });
---
+++ xuemo/www/module/app/chat/index.js
@@ -0,0 +1,27 @@
+angular.module('starter.controllers')
+
+    .controller('chatCtrl', ['$scope',
+        function($scope) {
+            $scope.chatList=[{
+                "content":"我要去打车海淀区中关村东路盈都大厦C座.......",
+                "time":"22:13",
+                "origin":"user",
+                "avatar":'http://bbs.iapps.im/uc_server/avatar.php?uid=127194&size=middle'
+            },
+                {
+                    "content":"正在帮您预定，费用大概45元左右",
+                    "avatar":'http://a1.mzstatic.com/us/r1000/105/Purple2/v4/d7/16/87/d7168712-1fa4-619b-304d-53fd50707893/mzl.qdrjvopz.175x175-75.jpg'
+                }
+            ];
+            $scope.chatContent='';
+            $scope.addChat=function(){
+                if($scope.chatContent==''){
+                    return;
+                }
+                $scope.chatList.push({
+                    "content":$scope.chatContent,
+                    "origin":"user"
+                });
+                $scope.chatContent='';
+            }
+        }])
