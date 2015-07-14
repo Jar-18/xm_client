7a7abfd9c80f058fe28ff24874e78ea7bf053046
Jar-18 <jar.an.cn@gmail.com>
2015-07-15 01:45:42 +0800

    Add cordova sqlite plugin for chat message local storage

--- xuemo/www/public/app.js
+++ xuemo/www/public/app.js
@@ -1,53 +1,73 @@
 // Ionic Starter App
 
 
-angular.module('starter.controllers', [    ])
+angular.module('starter.controllers', [])
 
 angular.module('starter', [
         'ionic',
         'starter.controllers',
 
         'service.config',
-        'service.chat'
+        'service.chat',
+
+        'ngCordova'
     ])
 
+    .run(function($ionicPlatform, $cordovaSQLite) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
 
-    .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
+            db = $cordovaSQLite.openDB("my.db");
+            $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS chat (id integer primary key, userId integer, latestMessage text, unReadCount integer, updatedAt datetime)");
+        });
+    })
 
-            // setup an abstract state for the tabs directive
-            .state('app', {
-                url: "/app",
-                views:{
-                    'app':{
-                        templateUrl: 'module/app/tab/index.html'
-                    }
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('app', {
+            url: "/app",
+            views: {
+                'app': {
+                    templateUrl: 'module/app/tab/index.html'
                 }
-            })
-            .state('chat', {
-                url: '/chat',
-                views: {
-                    'app': {
-                        templateUrl: 'module/app/chat/index.html',
-                        controller: 'chatCtrl'
-                    }
+            }
+        })
+        .state('chat', {
+            url: '/chat',
+            views: {
+                'app': {
+                    templateUrl: 'module/app/chat/index.html',
+                    controller: 'chatCtrl'
                 }
-            })
+            }
+        })
         // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/chat');
+    $urlRouterProvider.otherwise('/chat');
 
-    }).controller('mainCtrl', [
-        '$scope',
-        '$state',
-        'chatService', function ($scope, $state, chatService) {
-            var currentUserId=12;
-            var socket=chatService.init(currentUserId);
-            socket.on('private_message', function(data) {
-                console.debug(data);
-                $scope.$root.$broadcast('privateMessage',{
-                    userId:currentUserId,
-                    msg:data
-                });
+}).controller('mainCtrl', [
+    '$scope',
+    '$state',
+    'chatService',
+    function($scope, $state, chatService) {
+        var currentUserId = 12;
+        var socket = chatService.init(currentUserId);
+        socket.on('private_message', function(data) {
+            console.debug(data);
+            $scope.$root.$broadcast('privateMessage', {
+                userId: currentUserId,
+                msg: data
             });
-        }
-    ]);
+        });
+    }
+]);
